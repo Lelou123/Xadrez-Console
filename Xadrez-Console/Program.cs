5e07c46b12cd6ee0228e401aad17dfa52e830bda
Lelou123 <murillosama1@hotmail.com>
2022-03-17 18:08:43 -0300

    Posicao xadrez criada

--- Xadrez-Console/Program.cs
+++ Xadrez-Console/Program.cs
@@ -9,21 +9,8 @@ namespace Xadrez_Console
     {
         static void Main(string[] args)
         {
-            try
-            {
-                Tabuleiro tab = new Tabuleiro(8, 8);
-                
-                tab.ColocarPeca(new Torre(Cor.Preta, tab), new Posicao(0, 9));
-                tab.ColocarPeca(new Rei(Cor.Preta, tab), new Posicao(0, 0));
-                tab.ColocarPeca(new Torre(Cor.Preta, tab), new Posicao(1, 3));
-                tab.ColocarPeca(new Rei(Cor.Preta, tab), new Posicao(2, 4));
-
-                Tela.ImprimirTabuleiro(tab);
-            }
-            catch (TabuleiroException ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
+            PosicaoXadrez posx = new PosicaoXadrez('a', 1);
+            Console.WriteLine(posx);
             
             
             
---
+++ Xadrez-Console/xadrez/PosicaoXadrez.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xadrez_Console.xadrez
+{
+    internal class PosicaoXadrez
+    {
+        public char Coluna { get; set; }
+        public int Linha { get; set; }
+
+        public PosicaoXadrez(char coluna, int linha)
+        {
+            Coluna = coluna;
+            Linha = linha;
+        }
+        public override string ToString()
+        {
+            return "" + Coluna + Linha;
+        }
+    }
+}

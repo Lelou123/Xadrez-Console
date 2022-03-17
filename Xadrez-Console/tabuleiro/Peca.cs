8578a7729763d9274924b053b9f5a82729c7c897
Lelou123 <murillosama1@hotmail.com>
2022-03-17 15:56:23 -0300

    Adiciona tabuleiro peca e cor

---
+++ Xadrez-Console/tabuleiro/Peca.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tabuleiro;
+
+namespace Xadrez_Console.tabuleiro
+{
+    internal class Peca
+    {
+        public Posicao Posicao { get; set; }
+        public Cor Cor { get; protected set; }
+        public int QuantMovimetnos { get; set; }
+        public Tabuleiro Tabuleiro { get; set; }
+
+        public Peca(Posicao posicao, Cor cor, Tabuleiro tabuleiro)
+        {
+            Posicao = posicao;
+            Cor = cor;            
+            Tabuleiro = tabuleiro;
+            QuantMovimetnos = 0;
+        }
+
+    }
+}
